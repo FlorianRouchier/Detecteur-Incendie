8a23c11c0bf819db135f4816b1dacee3d48932fd
FlorianRouchier <95125890+FlorianRouchier@users.noreply.github.com>
2021-12-10 21:16:57 +0100

    Add files via upload

---
+++ smoke-sensor.ino
@@ -0,0 +1,20 @@
+
+const int sensorPin= A0;
+int smoke_level = -1; //Initialise le niveau de fumée à une valeur impossible au débogage. Si on voit la valeur -1 sur la sortie,
+                      //on sait que ce n'est pas une valeur lue par analalogRead()
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(sensorPin,INPUT);
+}
+
+void loop() {
+  smoke_level=analogRead(sensorPin); // MQ-2 est un capteur simple de résistance analogique
+  Serial.println(smoke_level);
+  if (smoke_level >120) {           //condition pour que le capteur soit en alerte
+    Serial.println("Fumée détectée");
+  }
+  delay(100);
+
+}
